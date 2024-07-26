31e374bf003676a75da8e49c78dd860058bf9061
isabellaim <iimartin@espol.edu.ec>
2024-07-25 19:13:57 -0500

    Creacion de proyecto

---
+++ patronesDisenoUML/src/main/java/espol/edu/ec/patronesdisenouml/PatronesDisenoUML.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package espol.edu.ec.patronesdisenouml;
+
+/**
+ *
+ * @author isabella
+ */
+public class PatronesDisenoUML {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
